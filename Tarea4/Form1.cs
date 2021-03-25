b6534a5f90d6dc63db010af87aa6d96bbee4ad23
programacion1umg@outlook.com <programacion1umg@outlook.com>
2021-03-25 12:13:21 -0600

    Cree las clases de Arboles

--- Tarea4/Form1.cs
+++ Tarea4/Form1.cs
@@ -16,5 +16,10 @@ namespace Tarea4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Comentario
+        }
     }
 }
